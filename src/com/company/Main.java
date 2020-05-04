70b98755af5bf29abf6504c35c9ac412d1de001e
Corey-Renton <33805998+Corey-Renton@users.noreply.github.com>
2020-05-04 15:52:47 +0100

    Cleaned up a little still need to make other classes but struggling with putting the While loops into Methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,90 +1,72 @@
 package com.company;
+
 import javafx.util.Pair;
 
 import java.io.*;
-import java.text.Collator;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import static java.util.Comparator.comparing;
 
-public class Main {
+class Main {
 
-
-    static final List<Pair<String, String>> words = new ArrayList<>();
-
-
+    private static final List<Pair<String, String>> PAIR_LIST = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
-
         Methods methods = new Methods();
+        int i = 0;
+        int y = 0;
+        int w = 0;
+        int Split_Amount = 2;
         String line;
         String[] column;
-        String[] dataHold = new String[200];
-        TreeMap<String, String> company_data = new TreeMap<>();
         int Amount_Of_Tuples = 0;
-
-        String value;
-
 
         String Input_File = "C:\\Users\\Yh\\Desktop\\CSVReader\\CSVReader\\src\\com\\company\\data.csv";
         File Output_File = new File("C:\\Users\\Yh\\Desktop\\CSVReader\\CSVReader\\src\\com\\company\\results.csv");
         BufferedReader br_input = new BufferedReader(new FileReader(Input_File));
 
-
         methods.File_Check(Output_File);
 
+            while ((line = br_input.readLine()) != null) {
 
-        while ((line = br_input.readLine()) != null) {
+                String Split_String = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"; // Had issues when initially splitting with "," due to the fact some columns included a , in addresses etc.
+                column = line.split(Split_String); // Split the 2 barrel string into 2 strings via the CSV ","
 
-            String Split_String = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"; // Had issues when initially splitting with "," due to the fact some columns included a , in addresses etc.
-            column = line.split(Split_String);
+                Pair<String, String> pair_input_tuples = new Pair<>(column[0], column[12]); // Pair value to load all tuples into the PAIR_LIST
+                PAIR_LIST.add(pair_input_tuples);
 
+                Amount_Of_Tuples++; // Have a counter here to provide accurate non hard-coded index's for our arrays.
+            }
 
-            Pair<String, String> paird = new Pair<>(column[0], column[12]);
-            words.add(paird);
+        String[] dataHold = new String[Amount_Of_Tuples * Split_Amount]; // Index should be the size of the Amount of Tuples needed times the amount of splits per string.
 
+        final Comparator<Pair<String, String>> comp = (comparing(Pair::getValue)); // Comparator to compare the Value key (Second String Value) in the PairList.
+        PAIR_LIST.sort(comp); // Sort PAIR_LIST by comparator we made this will sort our Key's aka our Company Country strings by Alphabetical Order.
 
-            System.out.println("Company Name: " + column[12] + " Company Country: " + column[0]); // Just for testing
-
-            Amount_Of_Tuples++;
-        }
-
-        final Comparator<Pair<String, String>> c = (comparing(Pair::getValue));
-        Collections.sort(words, c);
-
-//        words.stream()
-//                .sorted(c)
-//                .map(Pair::getValue)
-//                .forEach(System.out::println);
-
-        int y = 0;
-        int w = 0;
-        while(y < Amount_Of_Tuples) {
-            String str = words.get(y).toString();
-            String[] arrOfStr = str.split("=", 2);
-            for (String a : arrOfStr) {
+        while (y < Amount_Of_Tuples) {
+            String str = PAIR_LIST.get(y).toString();
+            String[] arrOfStr = str.split("=", Split_Amount); // Splits the PairList into two strings whilst keeping their bond (Value will always be +1 from the Key in the Array allowing us to keep their relation).
+            for (String a : arrOfStr) { //While there's a string in the array of strings add them to the DataHold array
                 dataHold[w] = a;
                 w++;
             }
             y++;
         }
 
-
-        int i = 0;
-        while(i < Amount_Of_Tuples * 2 - 2) {
+        while (i < Amount_Of_Tuples * 2 - 2) { // Minus 2 as we have two unnecessary tuples at the end. If have time
             try (FileWriter fw = new FileWriter(Output_File, true);
                  BufferedWriter br_output = new BufferedWriter(fw)) {
 
-
-                br_output.write(dataHold[i] + "," + dataHold[i+1]); // Writes the two needed columns to file.
+                br_output.write(dataHold[i] + "," + dataHold[i + 1]); // Writes the two needed columns to file.
                 br_output.newLine(); // New Tuple
-
 
             } catch (IOException e) {
                 System.err.format("IOException: %s%n", e);
             }
-            i = i +2;
+            i += Split_Amount; // Had to add two because adding one would mean 0 - 1 - 1 - 2  during the write syntax but adding 2 addresses this perfectly.
         }
 
     }
--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -3,15 +3,17 @@ package com.company;
 import java.io.File;
 import java.io.IOException;
 
-public class Methods {
+class Methods {
 
 
-    public void File_Check(File Output_File) throws IOException {
+    public void File_Check(File Output_File) throws IOException { // If File exists then delete else make new file
         if (Output_File.exists()) {
+            //(noinspection ResultOfMethodCallIgnored) <---- Please explain wtf this means IntelliJ put it here.
             Output_File.delete();
         } else {
             Output_File.createNewFile();
         }
+
 
 
     }
